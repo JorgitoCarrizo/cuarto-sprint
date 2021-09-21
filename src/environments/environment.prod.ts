c53bd7a018dcd32d592730e0ae0ff6d025712ca3
Jorge Carrizo <jorgitocarrizo@gmail.com>
2021-09-20 23:48:12 -0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDl66e9NBTbwp6trZxzp2L3zXJofRIQ9Ng",
+    authDomain: "basededatos-sala.firebaseapp.com",
+    databaseURL: "https://basededatos-sala-default-rtdb.firebaseio.com",
+    projectId: "basededatos-sala",
+    storageBucket: "basededatos-sala.appspot.com",
+    messagingSenderId: "236025211216",
+    appId: "1:236025211216:web:85e1a61726c7e527f52455",
+    measurementId: "G-RXJ3L7L6L9"
+  },
 };
